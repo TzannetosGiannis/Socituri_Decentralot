6742ccea13af8661c81193c1d96bab7727a5ba8b
Christos Hadjichristofi <christoshadjichristofi@hotmail.com>
2024-05-19 12:42:09 +0300

    Update crowd-funding.js

--- front-end/pages/crowd-funding.js
+++ front-end/pages/crowd-funding.js
@@ -5,7 +5,7 @@ const CrowdFundingPage = () => {
   return (
     <div className="bg-gray-800 min-h-screen flex justify-center">
       <div className="container mx-auto p-4">
-        <div className="w-9/12 mx-auto">
+        <div className="w-11/12 mx-auto">
           <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 justify-items-center">
             <CampaignCard />
             <CampaignCard />
