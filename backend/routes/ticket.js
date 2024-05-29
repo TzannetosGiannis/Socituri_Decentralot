974cf8f8360d4490c74f1c1bb3038f59e27120f3
TzannetosGiannis <itzannetos@mail.ntua.gr>
2024-05-29 13:52:39 +0300

    Update ticket.js
    WIP => return tickets for main lottery on default

--- backend/routes/ticket.js
+++ backend/routes/ticket.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const tickets = require('../models/ticket');
+const campaigns = require('../models/campaign');
 
 const router = express.Router();
 
@@ -8,7 +9,32 @@ async function get_all_tickets(req,res,next) {
     return res.json(allCamps);
 }
 
+async function get_tickets_per_round(req,res,next) {
+
+    let sender = req.params.sender;
+    let campaignId = req.params.campaignId;
+    let round = req.params.round;
+
+    if( campaignId == 'main' ) {
+        campaignId = (await campaigns.findOne({isCrowdFunding:false})).campaignId;
+    }
+
+    let ownedTickets = await tickets.find(
+        {
+            address:sender,
+            campaignId,
+            round
+        }
+    ).select('-__v -_id');
+
+    return res.send({
+        campaignId,
+        ownedTickets,
+        round
+    });
+}
 
 router.get('/fetch_all',get_all_tickets);
+router.get('/:sender/:campaignId/:round',get_tickets_per_round);
 
 module.exports = router;
