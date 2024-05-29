fd2d2acc153814058edad408d168d1fde4afb15d
TzannetosGiannis <itzannetos@mail.ntua.gr>
2024-05-29 15:30:25 +0300

    sync connection to systems

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,14 @@
 require('custom-env').env('localhost');
-const app = require('./app');
 const connectMongo = require('./utils/connectMongo');
 const connectSUI = require('./utils/connectSUI');
 
-const PORT = process.env.port || 3001;
+(async () => {
+  await connectMongo();
+  await connectSUI();
+  const app = require('./app');
 
-connectMongo();
-connectSUI();
-
-app.listen(PORT,'0.0.0.0', () => {
-  console.log(`REST API running at http://localhost:${ PORT }/`);
-});
+  const PORT = process.env.port || 3001;
+  app.listen(PORT,'0.0.0.0', () => {
+    console.log(`REST API running at http://localhost:${ PORT }/`);
+  });
+})();
