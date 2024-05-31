c17cf78d13adb286decd34415349ff57bb3d8331
TzannetosGiannis <itzannetos@mail.ntua.gr>
2024-05-31 11:30:51 +0300

    about page

--- front-end/data/teamMembers.js
+++ front-end/data/teamMembers.js
@@ -1,17 +1,30 @@
 export const teamMembers = [
     {
-        name: 'John Doe',
-        role: 'CEO',
-        description: 'John is the visionary leader of our company with over 20 years of experience.',
-        photo: '/path/to/john-doe.jpg',
-        linkedin: 'https://www.linkedin.com/in/johndoe'
+        name: 'Mantzoutas Andreas',
+        role: 'Chief Technology Officer',
+        description: 'Since the dawn of DeFi, Andreas has been an unwavering scholar and innovator, poised now to revolutionize the landscape.',
+        photo: '/assets/team/andreas_matzoutas.jpeg',
+        linkedin: 'https://www.linkedin.com/in/anmantzoutas/'
     },
     {
-        name: 'Jane Smith',
-        role: 'CTO',
-        description: 'Jane is the tech genius behind our product, leading our tech team with innovation.',
-        photo: '/path/to/jane-smith.jpg',
-        linkedin: 'https://www.linkedin.com/in/janesmith'
+        name: 'Hadjichristofi Christos',
+        role: 'Software Engineer',
+        description: 'With an exceptional grasp of software intricacies, Christos is the cornerstone of our journey towards transparency and technological excellence.',
+        photo: '/assets/team/christos_chatz.jpeg',
+        linkedin: 'https://www.linkedin.com/in/christoshadjichristofi/'
     },
-    // Add more team members here
+    {
+        name: 'Nikolaou George',
+        role: 'Product Owner',
+        description: 'An adept navigator of the blockchain domain, George brings a wealth of experience and a sharp intuition to our team’s innovative endeavors.',
+        photo: '/assets/team/george_nikolaou.png',
+        linkedin: 'https://www.linkedin.com/in/georgios-nikolaou-a58322180/'
+    },
+    {
+        name: 'Tzannetos Ioannis',
+        role: 'Chief Executive Officer',
+        description: 'At the helm of our protocol, Ioannis charts a course towards a permissionless future with visionary leadership and unwavering determination.',
+        photo: '/assets/team/tzannetos_ioannis.png',
+        linkedin: 'https://www.linkedin.com/in/giannis-tzannetos/'
+    }, 
 ];
